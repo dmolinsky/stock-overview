37f3f7cf86aad1f80561f774d7428329ae2e4b27
dmolinsky <diana.molinsky@iths.se>
2025-05-16 15:58:53 +0200

    Initial commit - Spring Boot stock sentiment analyzer

---
+++ src/main/java/se/dmolinsky/stocksentimentanalyze/StockSentimentAnalyzeApplication.java
@@ -0,0 +1,13 @@
+package se.dmolinsky.stocksentimentanalyze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSentimentAnalyzeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSentimentAnalyzeApplication.class, args);
+    }
+
+}
